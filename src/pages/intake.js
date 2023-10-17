bb5f7783804511c6467b20704e059f4f40ac0b04
purplepalm1 <110632274+purplepalm1@users.noreply.github.com>
2023-10-17 15:13:30 -0400

    implemented stepper, faulty

--- src/pages/intake.js
+++ src/pages/intake.js
@@ -4,12 +4,19 @@ import '../App.css'
 import '../index.css'
 import React from 'react';
 import { Link } from 'react-router-dom';
+import HorizontalLinearStepper from '../components/linearStepper';
 
 export const Intake = () => {
     return (
         <div className="App">
             <header className="App-header">
-
+                <div className='centered-heading'>
+                    <h1>Welcome to CreditCamel! <br />
+                        The revolutionary fintech tool to estimate your credit score.</h1>
+                </div>
+                <div className='stepper'>
+                    <HorizontalLinearStepper />
+                </div>
             </header>
         </div>
 
